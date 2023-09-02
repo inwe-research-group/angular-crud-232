a01b00fc1c5712a41994346b3965ad874f3c9325
inwe-research-group <zmamanir@unmsm.edu.pe>
2023-09-01 21:30:47 -0500

    version 1.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,62 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validator } from '@angular/forms';
+import { Contacto } from './modelos/contacto';
+import { ContactoService } from './servicios/contacto.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'angular-crud-232';
+  contactoArray: Contacto[] = [];
+  contactoForm: FormGroup;
+
+  constructor(
+    private formbuilder: FormBuilder,
+    private contactoService: ContactoService
+  ) {
+    this.contactoForm = formbuilder.group({
+      fullname: [''],
+      phone: [''],
+      email: [''],
+    });
+  }
+  ngOnInit(): void {
+    this.getContactos();
+  }
+
+  getContactos(): void {
+    this.contactoService.getContactos().subscribe(
+      (result: any) => {
+        this.contactoArray = result?.contactos;
+        console.log(this.contactoArray);
+      },
+      (err: any) => {
+        Swal.close();
+        Swal.fire({
+          icon: 'error',
+          title: 'Advertencia....',
+          text: '!Ah ocurrido un error!',
+        });
+      }
+    );
+  }
+  registrarContacto(): void {
+    this.contactoService.registrarContacto(this.contactoForm.value).subscribe(
+      (result: any) => {},
+      (err: any) => {
+        Swal.close();
+        Swal.fire({
+          icon: 'error',
+          title: 'Advertencia....',
+          text: '!Ah ocurrido un error al registrar!',
+        });
+      }
+    );
+    /*  console.log('llamando a getcontactos');
+    this.getContactos(); */
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
+import { ContactoService } from './servicios/contacto.service';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [ContactoService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
